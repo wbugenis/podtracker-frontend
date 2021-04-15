6bf038f21e656698e94c757b6735c4e972c3fed1
William Bugenis <wbugenis@gmail.com>
2021-04-15 17:54:23 -0400

    rss feed episode retrieval refactor complete

--- src/Episode.js
+++ src/Episode.js
@@ -1,35 +1,35 @@
 import React, {useEffect, useState} from "react"
 
-const Episode = ({user, episode, setCurrentTrack}) => {
+const Episode = ({user, episode, userEpisodes, podcastId, setCurrentTrack}) => {
     const [showDesc, setShowDesc] = useState(false)
-    const [userEpisode, setUserEpisode] = useState({listened: false, current_time:0})
-
-    const {id, title, description, runtime, filepath, filetype, pubDate} = episode
-    let myEp = episode.user_episodes.filter(episode => episode.user_id === user.id)[0]
-
-    useEffect(() => { 
-        if (myEp === undefined){
-            myEp = {listened: false, current_time:0}
+    const [userEpisode, setUserEpisode] = useState({listened: false, current_time:0, id:null})
+    const {title, description, runtime, filepath, pubDate} = episode
+    
+    useEffect(() => {
+        const myEp = userEpisodes.find(userEpisode => userEpisode.title === title)
+        console.log(myEp)
+        if (myEp){
+            console.log("ep found!")
+            setUserEpisode(myEp)
         }
-        setUserEpisode(myEp)
-    }, [])
+    }, [userEpisodes])
 
     const updateListened = () => {
-        if (myEp === undefined) {
+        if (userEpisode.id === null) {
             fetch('http://localhost:3000/user_episodes', {
                 method: 'POST',
                 headers: {
                     'Content-Type':'application/json',
                     Accept:'application/json'
                 },
-                body: JSON.stringify({user_id:user.id, episode_id:id, listened:!userEpisode.listened})
+                body: JSON.stringify({user_id:user.id, podcast_id:podcastId, listened:true, title:title})
             })
                 .then(r => r.json())
                 .then(userEp => setUserEpisode(userEp))
 
         } else {
 
-            fetch(`http://localhost:3000/user_episodes/${myEp.id}`, {
+            fetch(`http://localhost:3000/user_episodes/${userEpisode.id}`, {
                 method: 'PATCH',
                 headers: {
                     'Content-Type':'application/json',
@@ -39,18 +39,15 @@ const Episode = ({user, episode, setCurrentTrack}) => {
             })
                 .then(r => r.json())
                 .then(userEpisode => setUserEpisode(userEpisode)) 
-        } 
-                
-    }
-
-    
-
+        }
+    } 
+  
     return (
         <li style={userEpisode.listened ? {fontStyle:'italic'} : null}>
             <div>{title}</div>
             <div onClick={()=>setShowDesc(!showDesc)}>+ {showDesc ? <>{description}</> : null } </div>
             <div>{runtime} | {pubDate}</div>
-            <button onClick={()=>setCurrentTrack({title:title, src:filepath, type:filetype})}>PLAY</button>
+            <button onClick={()=>setCurrentTrack({title:title, src:filepath})}>PLAY</button>
             <br/>
             <button onClick={updateListened}>Listened</button>
         </li>
--- src/Podcast.js
+++ src/Podcast.js
@@ -1,19 +1,66 @@
 import React, {useState, useEffect} from "react"
+import Parser from 'rss-parser'
+//https://www.npmjs.com/package/rss-parser
 import Episode from "./Episode"
 
 const Podcast = ({user, unsubscribe, podcast, sub_id, setCurrentTrack}) => {
     const {id, title, rss_feed, description, podcast_img_url, podcast_home_url} = podcast
     const [episodes, setEpisodes] = useState([])
+    const [userEpisodes, setUserEpisodes] = useState([])
     const [showEpisodes, setShowEpisodes] = useState(false)
+    console.log(userEpisodes)
+    const prettyTime = (timeStr) => {
+        if (timeStr){
+            if (timeStr.includes(':')){
+                return timeStr
+            } else {
+                let seconds = parseInt(timeStr)
+
+                let hour = Math.floor(seconds/3600)+":"
+                if (hour === 0) {
+                    hour = ""
+                }
+                let minute = Math.floor(seconds%3600/60)
+                if(minute < 10){
+                    minute = "0" + minute
+                }
+                let second = Math.floor(seconds%60)
+                if (second < 10){
+                    second = "0" + second
+                }
+                return `${hour}${minute}:${second}`
+            }
+        } else return "Not provided"
+    }
 
     useEffect(() => {
-        fetch(`http://localhost:3000/podcasts/${id}/episodes`)
+        fetch(`http://localhost:3000/userepisodes/${user.id}/${podcast.id}`)
             .then(r => r.json())
-            .then(episodes => setEpisodes(episodes))
+            .then(userEpisodes => setUserEpisodes(userEpisodes))
+
+        fetch(`http://localhost:3000/podcasts/${id}/feed`)
+            .then(r => r.json())
+            .then(xmlString => {
+                const parser = new Parser()
+                return parser.parseString(xmlString.body)
+            })
+            .then(dom=> {
+                const episodes = []
+                dom.items.forEach(item => {
+                    episodes.push({
+                        title:item.title||"Not provided",
+                        description:item.content||"Not provided",
+                        runtime:prettyTime(item.itunes.duration),
+                        pubDate:item.pubDate.slice(0,16)||"Not provided",
+                        filepath:(item.enclosure ? item.enclosure.url : "Not provided")
+                    })
+                })
+                setEpisodes(episodes)
+            })
     }, [])
 
-    const episodeComps = episodes.map(episode => <Episode user={user} episode={episode} key={episode.title} setCurrentTrack={setCurrentTrack}/>)
-
+    const episodeComps = episodes.map(episode => <Episode user={user} episode={episode} key={episode.title} setCurrentTrack={setCurrentTrack} podcastId={podcast.id} userEpisodes={userEpisodes}/>)
+    
     return (
         <div className="podcast-div">
             <img src={podcast_img_url} alt={title}/>
