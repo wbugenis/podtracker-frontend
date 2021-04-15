506d622dad02833c67e55e866adcd4e608bdb069
William Bugenis <wbugenis@gmail.com>
2021-04-15 09:57:33 -0400

    player currentTime/stepping functionality working

--- src/Player.js
+++ src/Player.js
@@ -1,25 +1,43 @@
 import React, {useState} from "react"
-import { MediaPlayer } from '@cassette/player'
+import { MediaPlayerControls } from '@cassette/player'
+import { PlayerContextProvider } from '@cassette/core'
+import { usePlayerContext } from '@cassette/hooks'
 import '@cassette/player/dist/css/cassette-player.css'
 //https://github.com/benwiley4000/cassette#readme
 
+
+const PlayerContextUser = () => {
+    let { currentTime, onSeekComplete} = usePlayerContext(['currentTime', 'onSeekComplete'])
+    console.log(currentTime)
+    return (
+    <p>
+        {currentTime}
+        <button onClick={()=>onSeekComplete(currentTime-30)}>Back 30</button>
+        <button onClick={()=>onSeekComplete(currentTime+30)}>Add 30</button>
+    </p>)
+}
+
 const Player = ({currentTrack, setCurrentTrack}) => {
     const {title, src, type} = currentTrack
+
     console.log(src)
-    const playlist = [ {url:src, title:title}]
+    const playlist = [{url:src, title:title}]
     return (
         <div id="player-div">
-            <MediaPlayer 
-                playlist={playlist}
-                autoplay
-                controls={[
-                    'spacer',
-                    'playpause',
-                    'mute',
-                    'spacer',
-                    'progress'
-                  ]} />
-                  <br/>
+            <PlayerContextProvider playlist={playlist} playerContext={['currentTime', 'onSeekComplete']} > 
+                <MediaPlayerControls
+                    playlist={playlist}
+                    autoplay
+                    controls={[
+                        'spacer',
+                        'playpause',
+                        'mute',
+                        'spacer',
+                        'progress',
+                    ]} />
+                    <PlayerContextUser />
+            </PlayerContextProvider>
+            <br/>
         </div>
     )
 }
--- src/Podcast.js
+++ src/Podcast.js
@@ -4,6 +4,7 @@ import Episode from "./Episode"
 const Podcast = ({user, unsubscribe, podcast, sub_id, setCurrentTrack}) => {
     const {id, title, rss_feed, description, podcast_img_url, podcast_home_url} = podcast
     const [episodes, setEpisodes] = useState([])
+    const [showEpisodes, setShowEpisodes] = useState(false)
 
     useEffect(() => {
         fetch(`http://localhost:3000/podcasts/${id}/episodes`)
@@ -13,16 +14,23 @@ const Podcast = ({user, unsubscribe, podcast, sub_id, setCurrentTrack}) => {
 
     const episodeComps = episodes.map(episode => <Episode user={user} episode={episode} key={episode.title} setCurrentTrack={setCurrentTrack}/>)
 
-    return <div className="podcast-div">
-        <img src={podcast_img_url} alt={title}/>
-        <button onClick={()=>unsubscribe(sub_id)}>Unsubscribe</button>
-        <h4>{title}</h4>
-        <p>{description}</p>
-        <a href={podcast_home_url} target="_blank" rel="noreferrer">Homepage</a> 
-        <ul className="episode-holder">
-            {episodeComps}
-        </ul>       
-    </div>
+    return (
+        <div className="podcast-div">
+            <img src={podcast_img_url} alt={title}/>
+            <button onClick={()=>unsubscribe(sub_id)}>Unsubscribe</button>
+            <h4>{title}</h4>
+            <p>{description}</p>
+            <a href={podcast_home_url} target="_blank" rel="noreferrer">Homepage</a>
+            <button onClick={()=>setShowEpisodes(!showEpisodes)}>Show Episodes</button> 
+            {showEpisodes ? 
+                <ul className="episode-holder">
+                    {episodeComps}
+                </ul>
+                :
+                null
+            }
+        </div>
+    )
 }
 
 export default Podcast;
